b815b2bf9325245e27f6b5861fab01b2e93075fb
Tihomircho <tiho84@mail.bg>
2026-05-02 19:29:04 +0300

    fixed empty links

--- src/Components/Menu/Menu.tsx
+++ src/Components/Menu/Menu.tsx
@@ -4,7 +4,7 @@ const Menu: React.FC = () => {
   return (
     <nav className="navbar w-100 navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
-        <a className="navbar-brand" href="/">
+        <a className="navbar-brand" href="http://">
           Navbar
         </a>
         <button
@@ -21,17 +21,17 @@ const Menu: React.FC = () => {
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/">
+              <a className="nav-link active" aria-current="page" href="http://">
                 Home
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/">
+              <a className="nav-link" href="http://">
                 Features ksdjfghs
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/">
+              <a className="nav-link" href="http://">
                 Pricing
               </a>
             </li>
